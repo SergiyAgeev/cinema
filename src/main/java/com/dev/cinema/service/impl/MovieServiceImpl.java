1f510b3d14506b0c32d7503a5400c130d951025b
SergiyAgeev <sergiyageev@gmail.com>
2020-02-04 19:43:16 +0200

    relations #update1

--- src/main/java/com/dev/cinema/service/impl/MovieServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/MovieServiceImpl.java
@@ -3,10 +3,12 @@ package com.dev.cinema.service.impl;
 import com.dev.cinema.dao.MovieDao;
 import com.dev.cinema.exceptions.DataProcessingException;
 import com.dev.cinema.lib.Inject;
+import com.dev.cinema.lib.Service;
 import com.dev.cinema.model.Movie;
 import com.dev.cinema.service.MovieService;
 import java.util.List;
 
+@Service
 public class MovieServiceImpl implements MovieService {
     @Inject
     private static MovieDao movieDao;
