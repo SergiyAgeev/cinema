c714a1da97624f853ee0b11fcdaf8b53c40ab7af
SergiyAgeev <33694207+SergiyAgeev@users.noreply.github.com>
2020-02-05 11:50:33 +0200

    relations (#2)
    #update1

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -2,8 +2,15 @@ package com.dev.cinema;
 
 import com.dev.cinema.exceptions.DataProcessingException;
 import com.dev.cinema.lib.Injector;
+import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.model.Movie;
+import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.service.CinemaHallService;
 import com.dev.cinema.service.MovieService;
+import com.dev.cinema.service.MovieSessionService;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import org.apache.log4j.Logger;
 
 public class Main {
@@ -11,15 +18,40 @@ public class Main {
     private static Injector injector = Injector.getInstance("com.dev.cinema");
 
     public static void main(String[] args) throws DataProcessingException {
-        Movie movie = new Movie();
-        movie.setTittle("Fast and Furious");
         MovieService movieService = (MovieService)
                 injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTittle("Fast and Furious");
         movieService.add(movie);
         try {
             movieService.getAll().forEach(System.out::println);
         } catch (DataProcessingException e) {
             LOGGER.error("can't get all movies " + e);
         }
+
+        CinemaHallService cinemaHallService = (CinemaHallService)
+                injector.getInstance(CinemaHallService.class);
+
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(100);
+        cinemaHallService.add(cinemaHall);
+        try {
+            cinemaHallService.getAll().forEach(System.out::println);
+        } catch (DataProcessingException e) {
+            LOGGER.error("can't get all cinema halls" + e);
+        }
+
+        MovieSession movieSession = new MovieSession();
+        movieSession.setCinemaHall(cinemaHall);
+        movieSession.setMovie(movie);
+        movieSession.setShowTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(19, 30)));
+        MovieSessionService movieSessionService = (MovieSessionService)
+                injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(movieSession);
+
+        System.out.println("AVAILABLE SESSIONS");
+        movieSessionService.findAvailableSessions(movie.getId(), LocalDate.now())
+                .forEach(System.out::println);
     }
 }
---
+++ src/main/java/com/dev/cinema/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,51 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.MovieSessionDao;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.util.HibernateUtil;
+import java.time.LocalDate;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<MovieSession> query = session.createQuery(
+                    "From MovieSession where movie.id = :movieId"
+                            + " and year(showTime) = :year and month(showTime) = :month"
+                            + " and day(showTime) = :day");
+            query.setParameter("movieId", movieId);
+            query.setParameter("year", date.getYear());
+            query.setParameter("month", date.getMonthValue());
+            query.setParameter("day", date.getDayOfMonth());
+            return query.list();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get available sessions", e);
+        }
+    }
+
+    @Override
+    public MovieSession add(MovieSession movieSession) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Long movieSessionId = (Long) session.save(movieSession);
+            transaction.commit();
+            movieSession.setId(movieSessionId);
+            return movieSession;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add new movie session", e);
+
+        }
+    }
+}
