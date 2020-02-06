b0e122665f47679a48ec626225a3750783146a0e
SergiyAgeev <sergiyageev@gmail.com>
2020-02-06 18:21:09 +0200

    added Ticket & ShoppingCart

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -5,10 +5,12 @@ import com.dev.cinema.lib.Injector;
 import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.model.Movie;
 import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.model.User;
 import com.dev.cinema.service.AuthenticationService;
 import com.dev.cinema.service.CinemaHallService;
 import com.dev.cinema.service.MovieService;
 import com.dev.cinema.service.MovieSessionService;
+import com.dev.cinema.service.ShoppingCartService;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -24,14 +26,14 @@ public class Main {
                 injector.getInstance(MovieService.class);
 
         Movie movie = new Movie();
-        movie.setTittle("Fast and Furious");
+        movie.setTitle("Fast and Furious");
         movieService.add(movie);
         try {
             movieService.getAll().forEach(System.out::println);
         } catch (DataProcessingException e) {
             LOGGER.error("can't get all movies " + e);
         }
-
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         CinemaHallService cinemaHallService = (CinemaHallService)
                 injector.getInstance(CinemaHallService.class);
 
@@ -43,16 +45,16 @@ public class Main {
         } catch (DataProcessingException e) {
             LOGGER.error("can't get all cinema halls" + e);
         }
-
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         MovieSession movieSession = new MovieSession();
         movieSession.setCinemaHall(cinemaHall);
         movieSession.setMovie(movie);
         movieSession.setShowTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(19, 30)));
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         MovieSessionService movieSessionService = (MovieSessionService)
                 injector.getInstance(MovieSessionService.class);
         movieSessionService.add(movieSession);
 
-        System.out.println("AVAILABLE SESSIONS");
         movieSessionService.findAvailableSessions(movie.getId(), LocalDate.now())
                 .forEach(System.out::println);
 
@@ -60,6 +62,17 @@ public class Main {
                 injector.getInstance(AuthenticationService.class);
         authenticationService.register("sergiyageev@gmail.com", "123");
 
-        authenticationService.login("sergiyageev@gmail.com", "123");
+        User user = authenticationService.login("sergiyageev@gmail.com", "123");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+        ShoppingCartService shoppingCartService = (ShoppingCartService)
+                injector.getInstance(ShoppingCartService.class);
+        shoppingCartService.registerNewShoppingCart(user);
+        shoppingCartService.addSession(movieSession, user);
+        try {
+            shoppingCartService.getByUser(user).getTickets().forEach(System.out::println);
+        } catch (DataProcessingException e) {
+            LOGGER.error("can't get all tickets for user with id =  " + user.getId(), e);
+        }
     }
 }
---
+++ src/main/java/com/dev/cinema/service/impl/ShopingCartServiceImpl.java
@@ -0,0 +1,43 @@
+package com.dev.cinema.service.impl;
+
+import com.dev.cinema.dao.ShoppingCartDao;
+import com.dev.cinema.dao.TicketDao;
+import com.dev.cinema.lib.Inject;
+import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.model.ShoppingCart;
+import com.dev.cinema.model.Ticket;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.ShoppingCartService;
+import java.time.LocalDateTime;
+
+public class ShopingCartServiceImpl implements ShoppingCartService {
+    @Inject
+    private static TicketDao ticketDao;
+    @Inject
+    private static ShoppingCartDao shoppingCartDao;
+
+    @Override
+    public void addSession(MovieSession movieSession, User user) {
+        Ticket ticket = new Ticket();
+        ticket.setMovieSession(movieSession);
+        ticket.setUser(user);
+
+        ShoppingCart shoppingCart = shoppingCartDao.getByUser(user);
+        shoppingCart.getTickets().add(ticket);
+        shoppingCart.setLocalDateTime(LocalDateTime.now());
+        ticketDao.add(ticket);
+        shoppingCartDao.update(shoppingCart);
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        return shoppingCartDao.getByUser(user);
+    }
+
+    @Override
+    public void registerNewShoppingCart(User user) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUser(user);
+        shoppingCartDao.add(shoppingCart);
+    }
+}
